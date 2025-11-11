bd322f39fb7e322ec2b29d03990c59ddc39a5240
Annika Kaul Singh <annikaks@Annikas-MacBook-Pro.local>
2025-11-11 12:30:39 -0800

    initial results

--- config.py
+++ config.py
@@ -1,18 +1,18 @@
 import torch
 
 # PeMS-BAY 2022 file (.npz)
-DATA_PATH = "../CS230DeepLearningProject/data/PEMSBAY_2022.npy"  # TODO: change this
+DATA_PATH = "../CS230DeepLearningProject/data/PEMSBAY_2022.npy"  # TODO: make sure your env is setup like this
 
 # window + horizon
-WINDOW_SIZE = 12  # past 12*5min = 1 hour
-HORIZON = 1       # predict 1 step ahead
+WINDOW_SIZE = 12  # past 12, 5 min windows = each window is one hour
+HORIZON = 6       # predict 1 step (5 mins) ahead
 
-# splits (time-based)
+# splits (time-based)   
 TRAIN_RATIO = 0.7
-VAL_RATIO = 0.1   # test is 1 - TRAIN_RATIO - VAL_RATIO = 0.2
-                  # not sure if we should do train - 0.7, val - 0.15, test - 0.15
+VAL_RATIO = 0.1         # test is 1 - TRAIN_RATIO - VAL_RATIO = 0.2
+#TEST_RATIO = 0.2       # not sure if we should do train - 0.7, val - 0.15, test - 0.15
 
-# hyperparameters
+# hyperparameters, make this a grid search eventually
 BATCH_SIZE = 64
 LR = 1e-3
 EPOCHS = 10
